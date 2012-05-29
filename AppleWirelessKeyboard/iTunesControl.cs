ca775eb919b1484f65e721c74507b41fa6925cf9
gered <gered+github@blarg.ca>
2012-05-29 11:13:48 -0400

    disable iTunesLib related code for now
    This should be re-enabled later. I don't have this "iTunesLib" which I
    assume is installed along with iTunes on Windows (which I also don't
    have installed).

--- AppleWirelessKeyboard/iTunesControl.cs
+++ AppleWirelessKeyboard/iTunesControl.cs
@@ -3,38 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Diagnostics;
-using iTunesLib;
+//using iTunesLib;
 using System.Windows;
 
 namespace AppleWirelessKeyboard
 {
+	// TODO: get this "iTunesLib" (probably by installing iTunes?) and uncomment this code
+
     public static class iTunesControl
     {
         public static bool iTunesRunning
         {
             get
             {
-                var processes = Process.GetProcesses();
-                return Process.GetProcessesByName("iTunes").Any();
+                //var processes = Process.GetProcesses();
+                //return Process.GetProcessesByName("iTunes").Any();
+
+				// might as well just do this for now, since the iTunes functionality below
+				// has been commented out
+				return false;
             }
         }
         public static void NextSong()
         {
-            iTunesApp app = new iTunesApp();
-            app.NextTrack();
-            app = null;
+            //iTunesApp app = new iTunesApp();
+            //app.NextTrack();
+            //app = null;
         }
         public static void PreviousSong()
         {
-            iTunesApp app = new iTunesApp();
-            app.PreviousTrack();
-            app = null;
+            //iTunesApp app = new iTunesApp();
+            //app.PreviousTrack();
+            //app = null;
         }
         public static void PlayPause()
         {
-            iTunesApp app = new iTunesApp();
-            app.PlayPause();
-            app = null;
+            //iTunesApp app = new iTunesApp();
+            //app.PlayPause();
+            //app = null;
         }
     }
 }
